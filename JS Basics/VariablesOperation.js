a82ede4624896d3a987044feccb157304c19bb2e
Soham14-2005 <sohampsawant7@gmail.com>
2026-06-23 16:44:42 +0530

    Day 1: JavaScript Basics - Variables and Operations

---
+++ JS Basics/VariablesOperation.js
@@ -0,0 +1,21 @@
+const a=10;
+const b=20;
+const sum=a+b;
+const mul=a*b;
+const div=a/b;
+
+
+console.log(sum);
+console.log(mul);
+console.log(div);
+
+let x=20;
+console.log(x);
+x=70;
+console.log(x);
+
+let firstName="Soham";
+let lastName="Sawant";
+
+let fullName=firstName+" "+lastName;
+console.log(fullName);
